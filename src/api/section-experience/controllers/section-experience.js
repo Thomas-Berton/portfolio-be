7e5ab65e5ce8379fd854328f6f5b68583ed719c1
Thomas Berton <ekusas83@gmail.com>
2023-06-30 16:41:31 +0200

    add experience section

---
+++ src/api/section-experience/controllers/section-experience.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * section-experience controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::section-experience.section-experience');
---
+++ src/api/section-experience/routes/section-experience.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * section-experience router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::section-experience.section-experience');
